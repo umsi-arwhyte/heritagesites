b8154d01acccaed92af67ddcc2a96a3349fadab4
Anthony Whyte <anthwhyte@gmail.com>
2018-09-30 14:17:05 -0400

    Create heritagesites app, mysite project. heritagesites/models.py includes modified inspectdb models.  heritagesites/admin.py includes early view property re-org.

---
+++ mysite/urls.py
@@ -0,0 +1,39 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import path, include
+# from django.views.generic import RedirectView
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns = [
+    url(r'^$', lambda r: HttpResponseRedirect('heritagesites/')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^heritagesites/', include('heritagesites.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+'''
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('heritagesites/', include('heritagsites.urls')),
+]
+'''
+
