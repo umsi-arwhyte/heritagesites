2bc456bd2e9ecaf2040891bdb7b4ccae34a85d4c
Anthony Whyte <anthwhyte@gmail.com>
2018-11-20 11:14:16 -0500

    Update settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -109,9 +109,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-	    'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-        # 'rest_framework.permissions.AllowAny'
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
